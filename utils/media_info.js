f9200f0b69c8c32fe4cd91193b0edf46a64796ab
james58899 <james59988@gmail.com>
2017-08-26 11:44:22 +0800

    remove stream filter and fix PWRTelegram API mediainfo

--- utils/media_info.js
+++ utils/media_info.js
@@ -4,12 +4,14 @@ const fixPathname = require('../utils/fix_pathname');
 
 function parse(url) {
 	return new Promise(function (resolve, reject) {
-		execFile('ffprobe', [
+		execFile(require('ffmpeg-binaries').ffprobePath(), [
 			'-v', 'error',
 			'-of', 'default=nw=1',
-			'-show_entries', 'stream_tags=title,artist:format_tags=title,artist:format=duration',
+			'-show_entries', 'stream=codec_type:stream_tags=title,artist:format_tags=title,artist:format=duration',
 			fixPathname(url)
-		], { timeout: 10000 }, function (error, stdout, stderr) {
+		], {
+			timeout: 10000
+		}, function (error, stdout, stderr) {
 			if (error) {
 				if (error.code === 1) {
 					console.log('[MediaInfo] Error: Can\'t parse file ' + fixPathname(url));
@@ -18,24 +20,32 @@ function parse(url) {
 					console.log('[MediaInfo] Error: Fetch timeout ' + fixPathname(url));
 					return reject(new Error('Fetch timeout'));
 				}
-				return resolve({});
+				return reject(new Error(stderr));
 			}
 
-			if (stdout.match(/duration=(.*)/i)[1] == 'N/A') {
-				return reject(new Error('streaming source is not supported'));
+			if (!stdout.match('codec_type=audio')) {
+				reject(new Error('file is not audio'));
 			}
+
+			// if (stdout.match(/duration=(.*)/i)[1] == 'N/A') {
+			// 	console.log(stdout);
+			// 	return reject(new Error('streaming source is not supported'));
+			// }
 
 			var title;
 			var artist;
+			// var duration;
 			if (stdout.match(/TAG:title=(.*)/i) && stdout.match(/TAG:artist=(.*)/i)) {
 				title = stdout.match(/TAG:title=(.*)/i)[1];
 				artist = stdout.match(/TAG:artist=(.*)/i)[1];
+				// duration = stdout.match(/duration=(.*)/i)[1]; // TODO
 			}
 
 			if (artist && title) {
 				resolve({
 					title: title,
 					artist: artist
+					// duration: duration
 				});
 			} else {
 				resolve({});
